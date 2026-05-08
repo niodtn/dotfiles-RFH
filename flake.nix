5e551d985191a21c2396c92b3b4608561f2353a3
niodtn <ipegte93@gmail.com>
2026-05-09 04:50:19 +0900

    Refactor: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,34 @@
 {
-  description = "Niodtn's flake";
-
   inputs = {
-    # All
+    # --- 1. Core ---
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+    # --- 2. Platform Specific ---
+
+    # Darwin
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    mac-app-util.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-homebrew.inputs.nixpkgs.follows = "nixpkgs";
+
+    # # Asahi
+    # apple-silicon-support.url = "github:nix-community/nixos-apple-silicon";
+    # apple-silicon-support.inputs.nixpkgs.follows = "nixpkgs";
+
+    # # WSL
+    # nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    # nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+
+    # --- 3. etc ---
 
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";
@@ -22,20 +39,6 @@
 
     vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
-
-    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-
-    # NixOS
-    apple-silicon-support.url = "github:nix-community/nixos-apple-silicon";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    nix-gaming.url = "github:fufexan/nix-gaming";
-    # niri.url = "github:sodiboo/niri-flake";
-
-    # Darwin
-    nix-darwin.url = "github:nix-darwin/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    mac-app-util.url = "github:hraban/mac-app-util";
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
   outputs = inputs:
