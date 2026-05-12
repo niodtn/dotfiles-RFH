8c55eedcd351353e6662bc03d17b6ebc02851782
niodtn <ipegte93@gmail.com>
2026-05-13 01:34:19 +0900

    feat(networking): add firewall

--- nix/profiles/nixos/networking.nix
+++ nix/profiles/nixos/networking.nix
@@ -2,6 +2,7 @@
   networking = {
     hostName = config.hostName;
     networkmanager.enable = true;
+    firewall.enable = true;
   };
 
   users.users.${config.userName}.extraGroups = ["networkmanager"];
