7e39b6621a430d24be5bbe30b98f92bd9735935b
niodtn <ipegte93@gmail.com>
2026-05-19 00:02:53 +0900

    chore: caches

--- nix/lib/caches.nix
+++ nix/lib/caches.nix
@@ -2,14 +2,12 @@
   substituters = [
     "https://nix-community.cachix.org"
     "https://nix-darwin.cachix.org"
-    "https://nixos-apple-silicon.cachix.org"
-    "https://nix-gaming.cachix.org"
+    # "https://nixos-apple-silicon.cachix.org"
   ];
 
   trustedPublicKeys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "nix-darwin.cachix.org-1:LxMyKzQk7Uqkc1Pfq5uhm9GSn07xkERpy+7cpwc006A="
-    "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
-    "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    # "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
   ];
 }
