cb08e4526e6b1319eb02a0e21b89d9ab932861f4
niodtn <ipegte93@gmail.com>
2026-05-19 02:32:09 +0900

    refactor: nixos

--- nix/profiles/nixos/default.nix
+++ nix/profiles/nixos/default.nix
@@ -11,26 +11,6 @@
   ];
 
   # Nix
-  environment = {
-    shellAliases = {
-      resw = "sudo nixos-rebuild switch --flake ~/dotfiles#${config.hostName}";
-      rebo = "sudo nixos-rebuild boot --flake ~/dotfiles#${config.hostName}";
-      rebu = "nixos-rebuild build --flake ~/dotfiles#${config.hostName}";
-    };
-    systemPackages = [pkgs.xdg-utils];
-  };
-
-  programs.nix-ld.enable = true;
-
-  fonts.packages = [pkgs.noto-fonts-cjk-sans];
-
-  security.polkit.enable = true;
-
-  users.users.${config.userName} = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-  };
-
   services = {
     tailscale = {
       enable = true;
@@ -47,4 +27,26 @@
 
     dbus.enable = true;
   };
+
+  environment = {
+    shellAliases = {
+      resw = "sudo nixos-rebuild switch --flake ~/dotfiles#${config.hostName}";
+      rebo = "sudo nixos-rebuild boot --flake ~/dotfiles#${config.hostName}";
+      rebu = "nixos-rebuild build --flake ~/dotfiles#${config.hostName}";
+    };
+    systemPackages = [pkgs.xdg-utils];
+  };
+
+  users.users.${config.userName} = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+  };
+
+  security.polkit.enable = true;
+
+  documentation.nixos.enable = false;
+
+  programs.nix-ld.enable = true;
+
+  fonts.packages = [pkgs.noto-fonts-cjk-sans];
 }
