3568cd1dc920a5544287ec54db048fabe6d640cb
niodtn <ipegte93@gmail.com>
2026-05-21 03:52:25 +0900

    feat(ghostty): add GNOME compatibility

--- nix/modules/common/ghostty/linux.nix
+++ nix/modules/common/ghostty/linux.nix
@@ -3,15 +3,22 @@
   config,
   self,
   options,
+  pkgs,
   ...
 }: {
   config = lib.mkIf config.modules.ghostty (lib.mkMerge [
     (self.isLinux options (lib.mkMerge [
+      # Base
       {
         home-manager.users.${config.userName}.programs.ghostty = {
           systemd.enable = true;
         };
       }
+
+      # GNOME
+      (lib.mkIf (config.services.desktopManager.gnome.enable or false) {
+        environment.gnome.excludePackages = [pkgs.gnome-console];
+      })
     ]))
   ]);
 }
