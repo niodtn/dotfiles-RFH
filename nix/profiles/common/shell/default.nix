588207719c52c39a49219d071dc810f933bf9833
niodtn <ipete93@gmail.com>
2026-05-26 00:41:54 +0900

    fix: enableAllTerminfo

--- nix/profiles/common/shell/default.nix
+++ nix/profiles/common/shell/default.nix
@@ -1,4 +1,10 @@
-{config, ...}: {
+{
+  lib,
+  config,
+  self,
+  options,
+  ...
+}: {
   imports = [
     ./zsh.nix
 
@@ -7,39 +13,45 @@
     ./zellij.nix
   ];
 
-  # Nix
-  environment.enableAllTerminfo = true;
+  config = lib.mkMerge [
+    # Both
+    {
+      home-manager.users.${config.userName}.programs = {
+        atuin = {
+          enable = true;
+          enableZshIntegration = config.programs.zsh.enable;
+          settings = {
+            style = "auto";
+            invert = true;
+          };
+        };
 
-  # Home Manager
-  home-manager.users.${config.userName}.programs = {
-    atuin = {
-      enable = true;
-      enableZshIntegration = config.programs.zsh.enable;
-      settings = {
-        style = "auto";
-        invert = true;
+        direnv = {
+          enable = true;
+          silent = true;
+          nix-direnv.enable = true;
+          enableZshIntegration = config.programs.zsh.enable;
+        };
+
+        starship = {
+          enable = true;
+          enableZshIntegration = config.programs.zsh.enable;
+          settings = {
+            add_newline = false;
+            character.format = "❯ ";
+
+            line_break.disabled = true;
+            git_status.disabled = true;
+            package.disabled = true;
+            python.disabled = true;
+          };
+        };
       };
-    };
+    }
 
-    direnv = {
-      enable = true;
-      silent = true;
-      nix-direnv.enable = true;
-      enableZshIntegration = config.programs.zsh.enable;
-    };
-
-    starship = {
-      enable = true;
-      enableZshIntegration = config.programs.zsh.enable;
-      settings = {
-        add_newline = false;
-        character.format = "❯ ";
-
-        line_break.disabled = true;
-        git_status.disabled = true;
-        package.disabled = true;
-        python.disabled = true;
-      };
-    };
-  };
+    # Linux
+    (self.isLinux options {
+      environment.enableAllTerminfo = true;
+    })
+  ];
 }
